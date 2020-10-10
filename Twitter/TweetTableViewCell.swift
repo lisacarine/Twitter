6486100824c1b43343327057e562eb03392a067f
Lisa Fabien <lillyfab26@icloud.com>
2020-10-10 01:37:36 -0400

    Twitter App

---
+++ Twitter/TweetTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter
+//
+//  Created by Lisa Fabien on 10/9/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
